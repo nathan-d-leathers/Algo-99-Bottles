f05db3dc63a3cb105c118132df3624597df392a5
Nathan D Leathers <nathan.d.leathers@icloud.com>
2022-05-25 11:40:10 -0700

    solved using python

--- 99_bottles.py
+++ 99_bottles.py
@@ -1,5 +1,16 @@
-def bottle_song():
-	# write your code here!
+def bottle_song(num):
+    # write your code here!
+   
+    bottleCount = "bottles"
+    while num >= 2:
+        if num == 2:
+            bottleCount = "bottle"
+        newNum = num-1
+        print(f"{num} bottles of beer on the wall, {num} bottles of beer. Take one down and pass it around, {newNum} {bottleCount} of beer on the wall.")
+        num -= 1
+    if num == 1:
+        return(f"""1 bottle of beer on the wall, 1 bottle of beer. Take one down and pass it around, no more bottles of beer on the wall. 
+No more bottles of beer on the wall, no more bottles of beer. Go to the store and buy some more, 99 bottles of beer on the wall.""")
 
 
-bottle_song()
+print(bottle_song(10))
